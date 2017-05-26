9b9d3a79d082b26a4c4d8247e72eeeb25109a325
Pavel Haidzinski <Pavel.Haidzinski@thomsonreuters.com>
2017-05-26 18:20:06 +0300

    Added concurrentHashMap project

---
+++ ConcurrentHashMapExample/src/com/company/ConcurrentHashMapExample.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class ConcurrentHashMapExample {
+
+    public static void main(String[] args) {
+	// write your code here
+        //https://examples.javacodegeeks.com/core-java/util/concurrent/concurrenthashmap/java-util-concurrent-concurrenthashmap-example/
+    }
+}
